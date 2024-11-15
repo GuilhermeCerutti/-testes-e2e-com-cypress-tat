af1e8bbf4ee165ed4304047adcc888a346753e6a
Gui Cerutti <guilherme.cerutti@compliancestation.com.br>
2024-11-15 12:29:05 -0300

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    cypress: true // Adicione esta linha
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:cypress/recommended' // Adicione esta linha para regras específicas do Cypress
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    // Suas regras personalizadas, se houver
+  }
+};
