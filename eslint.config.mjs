8d7ebc77d763845505def16b7f2a5f2037234548
Gui Cerutti <guilherme.cerutti@compliancestation.com.br>
2024-11-15 12:26:15 -0300

    es-lint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,45 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-
-
-
-{
-  "env": {
-      "browser": true,
-      "commonjs": true,
-      "es2021": true,
-      "node": true
-  },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-      "ecmaVersion": 12
-  },
-  "rules": {
-      "indent": [
-          "error",
-          2
-      ],
-      "linebreak-style": [
-          "error",
-          "unix"
-      ],
-      "quotes": [
-          "error",
-          "single"
-      ],
-      "semi": [
-          "error",
-          "never"
-      ],
-      "no-trailing-spaces": [
-          "error"
-      ]
-  }
-}
-
 ];
