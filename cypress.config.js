f1b6aae1d4b21cd8a2c7bff246b03a8d200bd7f7
Gui Cerutti <guilherme.cerutti@compliancestation.com.br>
2024-11-15 12:12:02 -0300

    configur cypress clooud

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "idom4e",
   e2e: {
     chromeWebSecurity: false,
     baseUrl: 'https://notes-serverless-app.com',
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
